54b973a20d197050d8e0fd373a29f6f4eab0ad56
vijayakumar1069 <vijay.r20799@gmail.com>
2025-04-17 21:02:13 +0530

    bighil-complaints page converted into client components

--- src/app/(bighil)/bighil/(bighil dahboard)/bighil-complaints/page.js
+++ src/app/(bighil)/bighil/(bighil dahboard)/bighil-complaints/page.js
@@ -1,4 +1,12 @@
-import ComplaintFilter from "@/components/UI_Components/Standard_Components/ComplaintFilter";
+"use client";
+
+import dynamic from "next/dynamic";
+
+const ComplaintFilter = dynamic(
+  () =>
+    import("@/components/UI_Components/Standard_Components/ComplaintFilter"),
+  { ssr: false }
+);
 
 export default function BighilComplaintPage() {
   return (
