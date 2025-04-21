81811bbb18d845296f8e2771380d02ff61d38808
vijayakumar1069 <vijay.r20799@gmail.com>
2025-04-21 15:25:12 +0530

    pdf preview bug fixed by adding seperate file pdf.worker.js in public folder

--- src/components/UI_Components/Standard_Components/ComplaintsTable.jsx
+++ src/components/UI_Components/Standard_Components/ComplaintsTable.jsx
@@ -26,11 +26,11 @@ import { Document, Page, pdfjs } from "react-pdf";
 import { getBackendUrl } from "@/lib/getBackendUrl";
 import useAccessToken from "@/custom hooks/useAccessToken";
 import { cn } from "@/lib/utils";
-
-pdfjs.GlobalWorkerOptions.workerSrc = new URL(
-  "pdfjs-dist/build/pdf.worker.min.mjs",
-  import.meta.url
-).toString();
+// pdfjs.GlobalWorkerOptions.workerSrc = new URL(
+//   "pdfjs-dist/build/pdf.worker.min.mjs",
+//   import.meta.url
+// ).toString();
+pdfjs.GlobalWorkerOptions.workerSrc = "/pdf.worker.js";
 // Add to very top of ComplaintFilter.jsx
 
 const ComplaintsTable = ({
