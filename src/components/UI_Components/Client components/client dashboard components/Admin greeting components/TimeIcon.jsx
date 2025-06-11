24f646cc8a25e5cb129e194144f92565128a8bca
R.Vijayakumar <vijay.r20799@gmail.com>
2025-06-11 20:27:24 +0530

    admin greeting time icon colors changed based on timing

--- src/components/UI_Components/Client components/client dashboard components/Admin greeting components/TimeIcon.jsx
+++ src/components/UI_Components/Client components/client dashboard components/Admin greeting components/TimeIcon.jsx
@@ -1,9 +1,8 @@
 import { Moon, Sun, Sunrise, Sunset } from "lucide-react";
 
 export const TimeIcon = ({ hours }) => {
-  if (hours >= 5 && hours < 8)
-    return <Sunrise className="text-accent-warning" />;
-  if (hours >= 8 && hours < 18) return <Sun className="text-accent-warning" />;
-  if (hours >= 18 && hours < 21) return <Sunset className="text-accent-info" />;
+  if (hours >= 5 && hours < 8) return <Sunrise className="text-yellow" />;
+  if (hours >= 8 && hours < 18) return <Sun className="text-red" />;
+  if (hours >= 18 && hours < 21) return <Sunset className="text-black" />;
   return <Moon className="text-secondary-DEFAULT" />;
 };
